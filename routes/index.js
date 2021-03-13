fe4aec64d038e8914eb963093f5f51876debd360
GTIvan <ivberes@gmail.com>
2021-03-13 12:33:49 -0600

    first inital push

---
+++ routes/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+router.use("/app", require("./routes"));
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+//const dotenv = require ("dotenv")
+//dotenv.config()
+const routesUrls = require("./routes");
+const cors = require("cors");
+const DB = process.env.DATABASE_ACCESS;
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected"));
+app.use(routesUrls);
+app.listen(4000, () => console.log("server is up and running"));
