e8764a0e4bf59a05daf7d34c73d1a10a402b8ddc
GTIvan <ivberes@gmail.com>
2021-03-15 20:00:14 -0500

    added login link to the navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -10,6 +10,7 @@ function Navbar() {
       <ul>
       <li> <Link className="navbar-brand" to="/">Banksy </Link></li>
       <li><Link to="/" className={ window.location.pathname === "/" || window.location.pathname === "/home" ? "nav-link active" : "nav-link" }> Home </Link></li>
+      <li><Link to="/" className={ window.location.pathname === "/" || window.location.pathname === "/login" ? "nav-link active" : "nav-link" }> Login </Link></li>
       <li><Link to="/" className={ window.location.pathname === "/" || window.location.pathname === "/reviews" ? "nav-link active" : "nav-link" }> Reviews </Link></li>
       <li className="cart"><Link  to="/"><i className="fa fa-shopping-basket" aria-hidden="true"/> Cart: $0.00</Link></li>
       </ul>
