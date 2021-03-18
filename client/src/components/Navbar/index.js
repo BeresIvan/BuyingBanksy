6689233817883793e5b2c2b2134b8ef72626b56c
Brian Vargas <brian.vargas17@gmail.com>
2021-03-17 22:48:15 -0500

    added the logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./style.css";
 import {Navbar, Nav} from 'react-bootstrap';
+import Logo from "./BuyingBanksy.png"
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Toolbar() {
   return (
   <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Buying Banksy</Navbar.Brand>
+    <Navbar.Brand href="#home"><img src={Logo}/></Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="home">Home</Nav.Link>
       <Nav.Link href="gallery">Gallery</Nav.Link>
