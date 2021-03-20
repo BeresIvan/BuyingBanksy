54def1e3dff77d13e41b2b405deb315820a847a0
Brian Vargas <brian.vargas17@gmail.com>
2021-03-20 13:50:28 -0500

    styling changes to the navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,16 +6,16 @@ import Logo from "./BuyingBanksy.png"
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Toolbar() {
   return (
-  <Navbar bg="dark" variant="dark">
+  <Navbar>
     <Navbar.Brand href="home"><img src={Logo} alt="Buying Banksy"/></Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="home">Home</Nav.Link>
-      <Nav.Link href="gallery">Gallery</Nav.Link>
-      <Nav.Link href="Reviews">Reviews</Nav.Link>
-      <Nav.Link href="Cart">Cart</Nav.Link>
-      <Nav.Link href="Login">Login</Nav.Link>
-      <Nav.Link href="Signup">Signup</Nav.Link>
-      <Nav.Link href="Payment">Payment</Nav.Link>
+      <Nav.Link href="home" id="link">Home</Nav.Link>
+      <Nav.Link href="gallery" id="link">Gallery</Nav.Link>
+      <Nav.Link href="Reviews" id="link">Reviews</Nav.Link>
+      <Nav.Link href="Cart" id="link">Cart</Nav.Link>
+      <Nav.Link href="Login" id="link">Login</Nav.Link>
+      <Nav.Link href="Signup" id="link">Signup</Nav.Link>
+      <Nav.Link href="Payment" id="link">Payment</Nav.Link>
 
     </Nav>
   </Navbar>
