6671c045505396aa9273e2d5493004ec70cc57b1
GTIvan <ivberes@gmail.com>
2021-03-21 17:35:50 -0500

    udgated the links from local to heroku

--- client/src/components/LogOutButton/LogOutButton.js
+++ client/src/components/LogOutButton/LogOutButton.js
@@ -9,10 +9,10 @@ function LogOutBtn() {
   const history = useHistory();
 
   async function logOut() {
-    await axios.get("http://localhost:3000/logout");
-    // await axios.get(
-      // "https://buying-banksy.herokuapp.com//logout"
-    // );
+    //await axios.get("http://localhost:3000/logout");
+     await axios.get(
+       "https://buying-banksy.herokuapp.com//logout"
+     );
     await getLoggedIn();
     history.push("/Home");
   }
